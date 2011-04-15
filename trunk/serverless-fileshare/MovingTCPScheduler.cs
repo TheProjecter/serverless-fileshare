ff23482ab1f88a8928f86635e9a1ed0b9df31d90
calesherry@gmail.com <calesherry@gmail.com@c6fe0166-b91d-8532-9a60-b334f79913fe>
2011-04-15 02:51:52 +0000

    Added MovingTCPScheduler.cs which has a timer set for every 5000MS to change the ports on the listeners

---
+++ trunk/serverless-fileshare/MovingTCPScheduler.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net;
+
+namespace serverless_fileshare
+{
+    class MovingTCPScheduler
+    {
+        PortFinder _portFinder = new PortFinder();
+        PortListener[] _portListeners;
+        System.Windows.Forms.Timer _portChangeClock = new System.Windows.Forms.Timer();
+
+        public MovingTCPScheduler()
+        {
+            _portListeners = new PortListener[3];
+            _portChangeClock.Interval = Properties.Settings.Default.PortChangeInterval;
+            _portChangeClock.Tick += new EventHandler(timer_Tick);
+            
+        }
+
+        public void Start()
+        {
+            _portChangeClock.Start();
+            UpdateListeners();
+        }
+
+        public void Stop()
+        {
+            _portChangeClock.Stop();
+            StopListeners();
+        }
+
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            UpdateListeners();
+        }
+        
+
+        private void UpdateListeners()
+        {
+            StopListeners();
+
+            _portListeners[0] = new PortListener(IPAddress.Any, _portFinder.GetPreviousPort());
+            _portListeners[1] = new PortListener(IPAddress.Any, _portFinder.GetCurrentPort());
+            _portListeners[2] = new PortListener(IPAddress.Any, _portFinder.GetNextPort());
+
+            StartListeners();
+        }
+
+        private void StartListeners()
+        {
+            foreach (PortListener listener in _portListeners)
+            {
+                listener.Start();
+            }
+        }
+
+        private void StopListeners()
+        {
+            foreach (PortListener listener in _portListeners)
+            {
+                listener.Stop();
+            }
+        }
+    }
+}
